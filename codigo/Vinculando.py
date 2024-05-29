e4bd76ca18503c799c9b484ef8dd8d7bdcd95e3a
Diogo Borges de Moura <111078132+DigogSXD@users.noreply.github.com>
2024-05-29 12:29:57 -0300

    Create Vinculando.py

---
+++ codigo/Vinculando.py
@@ -0,0 +1,9 @@
+aulas_df = pd.DataFrame(columns=['Nome', 'Instrutor', 'Vagas', 'Alunos Matriculados'])
+
+# Exemplo de aulas disponíveis
+aulas_df = pd.DataFrame({
+    'Nome': ['Treinamento de Combate', 'Controle de Poderes'],
+    'Instrutor': ['Wolverine', 'Tempestade'],
+    'Vagas': [5, 3],
+    'Alunos Matriculados': [[], []]
+})
